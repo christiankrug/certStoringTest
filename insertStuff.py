34382327063e169d8f32abe0c049ccf3a3ba6b65
Christian Krug <mrminifreddy@gmail.com>
2022-04-28 13:29:40 +0200

    Improve randomness of entries inserted

--- insertStuff.py
+++ insertStuff.py
@@ -3,6 +3,7 @@ import json
 import random
 import time
 import datetime
+import copy
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["ctMonitor"]
@@ -21,11 +22,17 @@ dictionaries.append(json.load(github))
 while(True):
 	start = datetime.datetime.now()
 	for i in range(0,1000):
-		x = mycol.insert_many(dictionaries)
-		#pymongo adds the id field to the dictionary automatically, so we can reinsert the same stuff we need to delete that
-		del dictionaries[0]["_id"]
-		del dictionaries[1]["_id"]
-		del dictionaries[2]["_id"]
+		tempdics = []
+		for j in range(len(dictionaries)):
+			# pymongo adds the id field to the dictionary automatically, so we can reinsert the same stuff we need to delete that
+			if "_id" in dictionaries[j]:
+				del dictionaries[j]["_id"]
+			# make data a little more random
+			tempdics.append(copy.deepcopy(dictionaries[j]))
+			tempdics[j]["subject"]["common_name"] = tempdics[j]["subject"]["common_name"]+str(datetime.datetime.utcnow())
+			tempdics[j]["serial_number"] =  tempdics[j]["serial_number"]+str(datetime.datetime.utcnow())
+		x = mycol.insert_many(tempdics)
+		del tempdics
 	stop = datetime.datetime.now()
 	time = stop - start
 	print("Took us ", time.microseconds/1000, " ms for 3000 records")
